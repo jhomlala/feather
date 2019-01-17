d808ca70b13076da978e3252a95abb1c6da6dce3
Jakub  Homlala <jhomlala@gmail.com>
2019-01-17 22:11:39 +0100

    Weather API Provider unit tests

--- test/weather_api_provider_test.dart
+++ test/weather_api_provider_test.dart
@@ -3,6 +3,8 @@ import 'package:feather/src/models/remote/coordinates.dart';
 import 'package:feather/src/models/remote/main_weather_data.dart';
 import 'package:feather/src/models/remote/overall_weather_data.dart';
 import 'package:feather/src/models/remote/system.dart';
+import 'package:feather/src/models/remote/weather_forecast_list_response.dart';
+import 'package:feather/src/models/remote/weather_forecast_response.dart';
 import 'package:feather/src/models/remote/weather_response.dart';
 import 'package:feather/src/models/remote/wind.dart';
 import 'package:feather/src/resources/weather_api_provider.dart';
@@ -14,10 +16,10 @@ void main() {
     weatherApiProvider = WeatherApiProvider();
   });
 
-  group("Current weather test", ()  {
+  group("Current weather test", () {
     WeatherResponse response;
 
-    setUpAll(() async{
+    setUpAll(() async {
       response = await weatherApiProvider.fetchWeather(0, 0);
     });
 
@@ -29,10 +31,10 @@ void main() {
       System system = response.system;
       print(system.country);
       expect(system != null, true);
-      expect(system.sunset != null , true);
+      expect(system.sunset != null, true);
       expect(system.sunrise != null, true);
       expect(system.sunrise > 0, true);
-      expect(system.sunset > 0,true);
+      expect(system.sunset > 0, true);
     });
 
     test("Weather main data object not null and not empty", () {
@@ -43,17 +45,18 @@ void main() {
       expect(mainWeatherData.tempMax != null, true);
       expect(mainWeatherData.tempMin != null, true);
       expect(mainWeatherData.pressure > 0, true);
-      expect(mainWeatherData.temp > 0,true);
-      expect(mainWeatherData.humidity > 0 ,true);
-      expect(mainWeatherData.tempMax > 0,true);
-      expect(mainWeatherData.tempMin > 0,true);
+      expect(mainWeatherData.temp > 0, true);
+      expect(mainWeatherData.humidity > 0, true);
+      expect(mainWeatherData.tempMax > 0, true);
+      expect(mainWeatherData.tempMin > 0, true);
     });
 
     test("Overall weather data object not null and not empty", () {
-      List<OverallWeatherData> overallWeatherDataList = response.overallWeatherData;
+      List<OverallWeatherData> overallWeatherDataList =
+          response.overallWeatherData;
       expect(overallWeatherDataList != null, true);
       expect(overallWeatherDataList.length > 0, true);
-      for (OverallWeatherData overallWeatherData in overallWeatherDataList){
+      for (OverallWeatherData overallWeatherData in overallWeatherDataList) {
         expect(overallWeatherData.id != null, true);
         expect(overallWeatherData.description != null, true);
         expect(overallWeatherData.icon != null, true);
@@ -61,7 +64,7 @@ void main() {
       }
     });
 
-    test("Wind weather data object not null and not empty",(){
+    test("Wind weather data object not null and not empty", () {
       Wind wind = response.wind;
       expect(wind != null, true);
       expect(wind.speed != null, true);
@@ -70,13 +73,13 @@ void main() {
       expect(wind.speed >= 0, true);
     });
 
-    test("Clouds weather data object not null and not empty",(){
+    test("Clouds weather data object not null and not empty", () {
       Clouds clouds = response.clouds;
       expect(clouds != null, true);
       expect(clouds.all != null, true);
     });
 
-    test("Coordinates weather data object not null and not empty",(){
+    test("Coordinates weather data object not null and not empty", () {
       Coordinates coord = response.cord;
       expect(coord != null, true);
       expect(coord.longitude != null, true);
@@ -85,12 +88,36 @@ void main() {
       expect(coord.latitude >= -180 && coord.latitude <= 180, true);
     });
 
-    test("General weather data not null and not empty", (){
+    test("General weather data not null and not empty", () {
       expect(response.id != null, true);
       expect(response.name != null, true);
       expect(response.cod != null, true);
     });
+  });
 
+  group("Weather forecast test", () {
+    WeatherForecastListResponse response;
+    setUpAll(() async {
+      response =
+      await weatherApiProvider.fetchWeatherForecast(0, 0);
+    });
+
+    test("Weather forecast object not null and not empty", () {
+      expect(response != null, true);
+      expect(response.list != null, true);
+      expect(response.list.length > 0, true);
+    });
+
+    test("Weather forecast city object not null and not empty", () {
+      expect(response.city != null, true);
+      expect(response.city.name != null, true);
+      expect(response.city.id != null, true);
+    });
+
+    group("Weather forecast element object not null and noty empty", (){
+      WeatherForecastResponse responseElement = response.list[0];
+      
+    });
 
   });
 }
