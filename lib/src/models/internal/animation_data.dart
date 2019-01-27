ef16c3d115d6c1b20ed3c08f72d8972d3ff5594c
Jakub <jhomlala@gmail.com>
2019-01-27 18:33:38 +0100

    Removed unused class

--- lib/src/models/internal/animation_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/animation.dart';
-import 'package:rxdart/rxdart.dart';
-
-class AnimationData {
-  final AnimationController controller;
-  final Observable<double> observable;
-  AnimationData(this.controller, this.observable);
-}
