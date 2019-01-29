e16d9f1fbf4dd28026424bbed99dc62c8c332099
Jake Gough <super.jake@hotmail.co.uk>
2019-01-29 14:45:41 +0000

    Update application_colors.dart
    Added more colours for different parts of day/night cycle.

--- lib/src/resources/config/application_colors.dart
+++ lib/src/resources/config/application_colors.dart
@@ -3,10 +3,20 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ApplicationColors{
-  static final Color nightStartGradientColor = Color(0xFF4286f4);
-  static final Color nightEndGradient = Color(0xFF373B44);
-  static final Color dayStartGradientColor = Color(0x0fffdbb2d);
-  static final Color dayEndGradientColor = Color(0x0FF22c1c3);
+  static final Color midnightStartColor = Colors.black;
+  static final Color midnightEndColor = Colors.deepPurple.shade900;
+  static final Color nightStartColor = Colors.deepPurple.shade900;
+  static final Color nightEndColor = Colors.indigo;
+  static final Color twilightStartColor= Colors.indigo;
+  static final Color twilightEndColor = Colors.purple;
+  static final Color dawnDuskStartColor = Colors.indigo;
+  static final Color dawnDuskEndColod = Colors.pinkAccent;
+  static final Color morningEveStartColor = Colors.blue.shade700;
+  static final Color morningEveEndColor = Colors.pink.shade200;
+  static final Color dayStartColor = Colors.blue.shade700;
+  static final Color dayEndColor = Colors.lightBlue.shade300;
+  static final Color middayStartColor = Colors.blue;
+  static final Color middayEndColor = Colors.lightBlue.shade100;
   static final Color swiperInactiveDotColor = Colors.white54;
   static final Color swiperActiveDotColor = Colors.white;
 }
