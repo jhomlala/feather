94500f071e28c1edea6ccacccfd519a7aabd34c8
Jake Gough <super.jake@hotmail.co.uk>
2019-01-30 15:15:57 +0000

    Update gradient_cycle_test.dart
    Separated tests.

--- test/gradient_cycle_test.dart
+++ test/gradient_cycle_test.dart
@@ -60,35 +60,78 @@ void main() {
       return getDayGradient((testMs - sunriseMs) / (sunsetMs - sunriseMs));
     }
   }
-  test('testing gradient cycle', () {
+  
+  test('testing midnight before sunrise gradient', () {
     // Test > 0.6 for midnight
     expect(testGradientCycle(0, 0), buildGradient(ApplicationColors.midnightStartColor, ApplicationColors.midnightEndColor));
+  });
+
+  test('testing night before sunrise gradient', () {
     // Test < 0.6 for night
     expect(testGradientCycle(4, 0), buildGradient(ApplicationColors.nightStartColor, ApplicationColors.nightEndColor));
+  });
+
+  test('testing twilight before sunrise gradient', () {
     // Test < 0.2 for twilight
     expect(testGradientCycle(7, 0), buildGradient(ApplicationColors.twilightStartColor, ApplicationColors.twilightEndColor));
+  });
+
+  test('testing dawn before sunrise gradient', () {
     // Test < 0.1 for dawn before sunrise
     expect(testGradientCycle(7, 30), buildGradient(ApplicationColors.dawnDuskStartColor, ApplicationColors.dawnDuskEndColor));
+  });
+
+  test('testing dawn after sunrise gradient', () {
     // Test < 0.1 for for dawn after sunrise
     expect(testGradientCycle(8, 30), buildGradient(ApplicationColors.dawnDuskStartColor, ApplicationColors.dawnDuskEndColor));
+  });
+
+  test('testing morning after sunrise gradient', () {
     // Test < 0.2 for morning
     expect(testGradientCycle(10, 0), buildGradient(ApplicationColors.morningEveStartColor, ApplicationColors.morningEveEndColor));
+  });
+
+  test('testing day before midday gradient', () {
     // Test < 0.4 for day
     expect(testGradientCycle(11, 0), buildGradient(ApplicationColors.dayStartColor, ApplicationColors.dayEndColor));
+  });
+
+  test('testing midday gradient', () {
     // Test > 0.4 and < 0.6 for midday
     expect(testGradientCycle(13, 0), buildGradient(ApplicationColors.middayStartColor, ApplicationColors.middayEndColor));
+  });
+
+  test('testing day after midday gradient', () {
     // Test > 0.6 for day
     expect(testGradientCycle(14, 0), buildGradient(ApplicationColors.dayStartColor, ApplicationColors.dayEndColor));
+  });
+
+  test('testing evening before sunset gradient', () {
     // Test > 0.8 for evening
     expect(testGradientCycle(16, 30), buildGradient(ApplicationColors.morningEveStartColor, ApplicationColors.morningEveEndColor));
+  });
+
+  test('testing dusk before sunset gradient', () {
     // Test > 0.9 for dusk before sunset
     expect(testGradientCycle(17, 30), buildGradient(ApplicationColors.dawnDuskStartColor, ApplicationColors.dawnDuskEndColor));
+  });
+
+  test('testing dusk after sunset gradient', () {
     // Test < 0.1 for for dawn after sunset
     expect(testGradientCycle(18, 30), buildGradient(ApplicationColors.dawnDuskStartColor, ApplicationColors.dawnDuskEndColor));
+  });
+
+  test('testing twilight after sunset gradient', () {
     // Test < 0.2 for twilight
     expect(testGradientCycle(19, 0), buildGradient(ApplicationColors.twilightStartColor, ApplicationColors.twilightEndColor));
+  });
+
+  test('testing night after sunset gradient', () {
     // Test < 0.6 for
     expect(testGradientCycle(20, 0), buildGradient(ApplicationColors.nightStartColor, ApplicationColors.nightEndColor));
+  });
+
+  test('testing midnight after sunset gradient', () {
     // Test > 0.6 for midnight
     expect(testGradientCycle(23, 0), buildGradient(ApplicationColors.midnightStartColor, ApplicationColors.midnightEndColor));
   });
