e4d14fa2f414d84c1d91c6352507beba1b81f58f
Jakub <jhomlala@gmail.com>
2019-02-23 21:08:38 +0100

    Fixed tests

--- test/weather_main_page_test.dart
+++ test/weather_main_page_test.dart
@@ -3,16 +3,21 @@ import 'package:feather/src/models/remote/overall_weather_data.dart';
 import 'package:feather/src/models/remote/system.dart';
 import 'package:feather/src/models/remote/weather_response.dart';
 import 'package:feather/src/models/remote/wind.dart';
+import 'package:feather/src/ui/screen/about_screen.dart';
 import 'package:feather/src/ui/screen/weather_main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'test_helper.dart';
+
 main() {
   testWidgets("Weather main page should show widgets",
       (WidgetTester tester) async {
-    await tester.pumpWidget(WeatherMainPage(
+    await tester
+        .pumpWidget(TestHelper.wrapWidgetWithLocalizationApp(WeatherMainPage(
       weatherResponse: setupWeatherResponse(),
-    ));
+    )));
+
     expect(
         find.byKey(Key("weather_main_page_current_weather")), findsOneWidget);
   });
